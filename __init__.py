ac6fe66d2c01d8339504e4b0085b5cf2c19d3d1a
Felix Cheruiyot <felix.cheruiyot@kenyaapps.net>
2017-09-26 14:33:44 +0300

    Change app name

--- __init__.py
+++
@@ -1,9 +0,0 @@
-from client import *
-
-class PesaBot(object):
-    def __init__(self, auth):
-        self.client = Client(auth.get('email'), auth.get('password'))
-
-    def call(self, path, method='GET', payload=None):
-        return self.client.call(path, method, payload)
-    
