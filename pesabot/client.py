c096b258e46a671f7a26fb9b6914f0d9ea7dd2c8
Felix Cheruiyot <felix.cheruiyot@kenyaapps.net>
2017-09-27 11:32:20 +0300

    Added PUT and DELETE Methods

--- pesabot/client.py
+++ pesabot/client.py
@@ -12,6 +12,10 @@ class Client(object):
     def call(self, path, method, payload={}):
         if method == 'POST':
             res = requests.post(BASE_URL+path, json=payload, auth=self.auth)
+        elif method == 'PUT':
+            res = requests.put(BASE_URL+path, json=payload, auth=self.auth)
+        elif method == 'DELETE':
+            res = requests.delete(BASE_URL+path, auth=self.auth)
         else:
             res = requests.get(BASE_URL+path, auth=self.auth)
         return {'data': res.json(), 'status': res.status_code} 
